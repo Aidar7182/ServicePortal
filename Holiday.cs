fa4f4543c8fc504e1e16a1e2347b5e31a4383c96
Aidar7182 <47495659+Aidar7182@users.noreply.github.com>
2021-11-29 10:35:43 +0600

    Add files via upload

---
+++ Holiday.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Models
+{
+    public class Holiday
+    {
+        public int ID { set; get; }
+        public int EmployeeID { set; get; }
+
+        public string FromDate { set; get; }
+        public int Day { set; get; }
+        public string Status { set; get; }
+        public List<Holiday> holidays { set; get; }
+
+    }
+}
