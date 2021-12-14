ddc5b33bbf2ff80624debf62178e5f285de7781a
Gadaybaev Miristan <59552244+Miristan7@users.noreply.github.com>
2021-12-14 13:49:40 +0600

    Add files via upload
    Панель руководителя и формирование отчетов

---
+++ SupervisorClass.cs
@@ -0,0 +1,227 @@
+﻿using Portal.Controllers;
+using Portal.DB;
+using Portal.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Context
+{
+    public class SupervisorClass
+    {
+        readonly DBConn conn = new DBConn();
+
+        //ABSENCE
+        public List<AbsenseLog> GetAbsenseLogWorkers()
+        {
+            try
+            {
+
+                List<AbsenseLog> list = new List<AbsenseLog>();
+                conn.connect().Open();
+                using (SQLiteCommand fmd = conn.connect().CreateCommand())
+                {
+                    fmd.CommandText = @"select AbsenceLog.ID, AbsenceLog.EmployeeID, Employee.FullName, Department.DepartName, AbsenceLog.Reason, AbsenceLog.Date, AbsenceLog.Status  from AbsenceLog
+                                        JOIN Employee ON Employee.ID = AbsenceLog.EmployeeID
+                                        JOIN Department ON Department.ID = Employee.DepartmentID
+                                        where AbsenceLog.Status = " + "'На рассмотрении'";
+                    fmd.CommandType = CommandType.Text;
+                    SQLiteDataReader r = fmd.ExecuteReader();
+                    while (r.Read())
+                    {
+                        AbsenseLog obj = new AbsenseLog();
+                        obj.ID = Convert.ToInt32(r["ID"]);
+                        obj.EmployeeID = Convert.ToInt32(r["EmployeeID"]);
+                        obj.FullName = (Convert.ToString(r["FullName"]));
+                        obj.DepartName = (Convert.ToString(r["DepartName"]));
+                        obj.Reason = (Convert.ToString(r["Reason"]));
+                        obj.Date = (Convert.ToString(r["Date"]));
+                        obj.Status = (Convert.ToString(r["Status"]));
+                        list.Add(obj);
+                    }
+
+                }
+                return list;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                conn.connect().Close();
+            }
+        }
+
+        public List<AbsenseLog> GetAbsenseLogWorkersArchive()
+        {
+            try
+            {
+
+                List<AbsenseLog> list = new List<AbsenseLog>();
+                conn.connect().Open();
+                using (SQLiteCommand fmd = conn.connect().CreateCommand())
+                {
+                    fmd.CommandText = @"select AbsenceLog.ID, AbsenceLog.EmployeeID, Employee.FullName, Department.DepartName, AbsenceLog.Reason, AbsenceLog.Date, AbsenceLog.Status  from AbsenceLog
+                                        JOIN Employee ON Employee.ID = AbsenceLog.EmployeeID
+                                        JOIN Department ON Department.ID = Employee.DepartmentID
+                                        where AbsenceLog.Status = " + "'Отклонено'"+ " or AbsenceLog.Status = " + "'Утверждено'";
+                    fmd.CommandType = CommandType.Text;
+                    SQLiteDataReader r = fmd.ExecuteReader();
+                    while (r.Read())
+                    {
+                        AbsenseLog obj = new AbsenseLog();
+                        obj.ID = Convert.ToInt32(r["ID"]);
+                        obj.EmployeeID = Convert.ToInt32(r["EmployeeID"]);
+                        obj.FullName = (Convert.ToString(r["FullName"]));
+                        obj.DepartName = (Convert.ToString(r["DepartName"]));
+                        obj.Reason = (Convert.ToString(r["Reason"]));
+                        obj.Date = (Convert.ToString(r["Date"]));
+                        obj.Status = (Convert.ToString(r["Status"]));
+                        list.Add(obj);
+                    }
+
+                }
+                return list;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                conn.connect().Close();
+            }
+        }
+
+        public void UpdateAbsense(AbsenseLog obj)
+        {
+
+
+            using (SQLiteConnection connection = conn.connect())
+            {
+                connection.Open();
+                using (SQLiteCommand command = new SQLiteCommand(connection))
+                {
+                    command.CommandText = "update AbsenceLog set Status =:status where ID=:id";
+                    command.Parameters.Add("status", DbType.String).Value = obj.Status;
+                    command.Parameters.Add("id", DbType.String).Value = obj.ID;
+             
+
+                    command.ExecuteNonQuery();
+                }
+
+            }
+
+        }
+
+        //HOLIDAY
+        public List<Holiday> GetHolidayWorkers()
+        {
+            try
+            {
+
+                List<Holiday> list = new List<Holiday>();
+                conn.connect().Open();
+                using (SQLiteCommand fmd = conn.connect().CreateCommand())
+                {
+                    fmd.CommandText = @"select Holiday.ID, Holiday.EmployeeID, Employee.FullName, Department.DepartName, Holiday.FromDate, Holiday.Day, Holiday.Status  from Holiday
+                                        JOIN Employee ON Employee.ID = Holiday.EmployeeID
+                                        JOIN Department ON Department.ID = Employee.DepartmentID
+                                        where Holiday.Status = " + "'На рассмотрении'";
+                    fmd.CommandType = CommandType.Text;
+                    SQLiteDataReader r = fmd.ExecuteReader();
+                    while (r.Read())
+                    {
+                        Holiday obj = new Holiday();
+                        obj.ID = Convert.ToInt32(r["ID"]);
+                        obj.EmployeeID = Convert.ToInt32(r["EmployeeID"]);
+                        obj.FullName = (Convert.ToString(r["FullName"]));
+                        obj.DepartName = (Convert.ToString(r["DepartName"]));
+                        obj.FromDate = (Convert.ToString(r["FromDate"]));
+                        obj.Day = (Convert.ToInt32(r["Day"]));
+                        obj.Status = (Convert.ToString(r["Status"]));
+                        list.Add(obj);
+                    }
+
+                }
+                return list;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                conn.connect().Close();
+            }
+        }
+
+        public List<Holiday> GetHolidayWorkersArchive()
+        {
+            try
+            {
+
+                List<Holiday> list = new List<Holiday>();
+                conn.connect().Open();
+                using (SQLiteCommand fmd = conn.connect().CreateCommand())
+                {
+                    fmd.CommandText = @"select Holiday.ID, Holiday.EmployeeID, Employee.FullName, Department.DepartName, Holiday.FromDate, Holiday.Day, Holiday.Status  from Holiday
+                                        JOIN Employee ON Employee.ID = Holiday.EmployeeID
+                                        JOIN Department ON Department.ID = Employee.DepartmentID
+                                        where Holiday.Status = " + "'Отклонено'" + " or Holiday.Status = " + "'Утверждено'";
+                    fmd.CommandType = CommandType.Text;
+                    SQLiteDataReader r = fmd.ExecuteReader();
+                    while (r.Read())
+                    {
+                        Holiday obj = new Holiday();
+                        obj.ID = Convert.ToInt32(r["ID"]);
+                        obj.EmployeeID = Convert.ToInt32(r["EmployeeID"]);
+                        obj.FullName = (Convert.ToString(r["FullName"]));
+                        obj.DepartName = (Convert.ToString(r["DepartName"]));
+                        obj.FromDate = (Convert.ToString(r["FromDate"]));
+                        obj.Day = (Convert.ToInt32(r["Day"]));
+                        obj.Status = (Convert.ToString(r["Status"]));
+                        list.Add(obj);
+                    }
+
+                }
+                return list;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                conn.connect().Close();
+            }
+        }
+
+        public void UpdateHoliday(Holiday obj)
+        {
+
+
+            using (SQLiteConnection connection = conn.connect())
+            {
+                connection.Open();
+                using (SQLiteCommand command = new SQLiteCommand(connection))
+                {
+                    command.CommandText = "update Holiday set Status =:status where ID=:id";
+                    command.Parameters.Add("status", DbType.String).Value = obj.Status;
+                    command.Parameters.Add("id", DbType.String).Value = obj.ID;
+
+
+                    command.ExecuteNonQuery();
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ SupervisorController.cs
@@ -0,0 +1,140 @@
+﻿using Portal.Context;
+using Portal.DB;
+using Portal.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Portal.Controllers
+{
+    public class SupervisorController : Controller
+    {
+        readonly SupervisorClass supervisorClass  = new SupervisorClass();
+        readonly WorkerClass workerClass = new WorkerClass();
+        readonly EmployeeClass employeeClass = new EmployeeClass();
+        public static int val;
+
+        DBConn conn = new DBConn();
+
+
+        // GET: AbsenseList
+
+        public ActionResult AbsenseList()
+        {
+            AbsenseLog objs = new AbsenseLog();
+            objs.absence = supervisorClass.GetAbsenseLogWorkers().ToList();
+            return View(objs);
+
+        }
+
+        public ActionResult AbsenseListArchive()
+        {
+            AbsenseLog objs = new AbsenseLog();
+            objs.absence = supervisorClass.GetAbsenseLogWorkersArchive().ToList();
+            return View(objs);
+
+        }
+
+        // GET: SupervisorController/AbsenseConfirm/5
+        public ActionResult AbsenseConfirm(int id, int emp)
+        {
+            AbsenseLog objs = new AbsenseLog();
+
+            objs.absence = workerClass.GetAbsenseLogByWorkerID(emp).ToList();
+            return View(objs);
+
+        }
+
+        // POST: SupervisorController/AbsenseConfirm/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AbsenseConfirm([Bind] AbsenseLog obj)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+
+
+                    supervisorClass.UpdateAbsense(obj);
+                    return RedirectToAction("AbsenseList");
+
+
+                }
+                return View(obj);
+
+
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        // GET: HolidayList
+        public ActionResult HolidayList()
+        {
+            Holiday objs = new Holiday();
+            objs.holidays = supervisorClass.GetHolidayWorkers().ToList();
+            return View(objs);
+
+        }
+
+        public ActionResult HolidayListArchive()
+        {
+            Holiday objs = new Holiday();
+            objs.holidays = supervisorClass.GetHolidayWorkersArchive().ToList();
+            return View(objs);
+
+        }
+
+        // GET: SupervisorController/HolidayConfirm/5
+        public ActionResult HolidayConfirm(int id, int emp)
+        {
+            Holiday objs = new Holiday();
+
+            objs.holidays = workerClass.GetHolidayByWorkerID(emp).ToList();
+            return View(objs);
+
+        }
+
+        // POST: SupervisorController/HolidayConfirm/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HolidayConfirm([Bind] Holiday obj)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+
+
+                    supervisorClass.UpdateHoliday(obj);
+                    return RedirectToAction("HolidayList");
+
+
+                }
+                return View(obj);
+
+
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
